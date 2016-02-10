7eadc178773355c021f1f5bc494689371a63ea4c
MohammedAkram <makram01@nyit.edu>
2016-02-09 21:28:08 -0500

    help
    plez

---
+++ test/test.Droid/obj/Debug/android/src/md52d092a3298bfdf816f326cc672685199/restaurantlayoutActivity1.java
@@ -0,0 +1,56 @@
+package md52d092a3298bfdf816f326cc672685199;
+
+
+public class restaurantlayoutActivity1
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		android.view.View.OnTouchListener
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onTouch:(Landroid/view/View;Landroid/view/MotionEvent;)Z:GetOnTouch_Landroid_view_View_Landroid_view_MotionEvent_Handler:Android.Views.View/IOnTouchListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("test.Droid.restaurantlayoutActivity1, test.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", restaurantlayoutActivity1.class, __md_methods);
+	}
+
+
+	public restaurantlayoutActivity1 () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == restaurantlayoutActivity1.class)
+			mono.android.TypeManager.Activate ("test.Droid.restaurantlayoutActivity1, test.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onTouch (android.view.View p0, android.view.MotionEvent p1)
+	{
+		return n_onTouch (p0, p1);
+	}
+
+	private native boolean n_onTouch (android.view.View p0, android.view.MotionEvent p1);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ test/test.Droid/restaurantlayoutActivity1.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace test.Droid
+{
+    [Activity(Label = "restaurantlayoutActivity1")]
+    public class restaurantlayoutActivity1 : Activity, View.IOnTouchListener
+    {
+        ImageView imageButton1;
+        override
+        
+
+        protected  void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Restauantlayout1);
+            // Create your application here
+            imageButton1 = (ImageView)FindViewById(Resource.Id.imageButton1);
+            imageButton1.SetOnTouchListener(this);
+        }
+        float x, y = 0f;
+        Boolean moving=false;
+        public bool OnTouch(View v, MotionEvent e)
+        {
+           
+            switch (e.Action)
+            {
+                case MotionEventActions.Down:
+                    moving = true;
+                    break;
+                case MotionEventActions.Move:
+                    if (moving)
+                    {
+                        x = e.RawX - imageButton1.Width / 2;
+                        y = e.RawY - imageButton1.Width ;
+                        imageButton1.SetX(x);
+                        imageButton1.SetY(y);
+                    }
+                    break;
+                case MotionEventActions.Up:
+                    moving = false;
+                    break;
+            }
+            return true;
+        }
+    }
+}
