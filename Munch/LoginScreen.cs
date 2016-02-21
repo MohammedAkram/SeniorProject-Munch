b6825544fe4f9273897b0631b04b2dbfa612b0a3
mirniaz <metorea@gmail.com>
2016-02-20 23:51:47 -0500

    Shit

--- Munch/LoginScreen.cs
+++ Munch/LoginScreen.cs
@@ -1,30 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System;
 using Android.App;
 using Android.Content;
-using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.OS;
 
 namespace Munch
 {
-    [Activity(Label = "LoginScreen", Theme="@style/android:Theme.Holo.Light.NoActionBar")]
+    [Activity(Label = "Munch", Icon = "@drawable/icon", Theme = "@android:style/Theme.Holo.Light.NoActionBar")]
     public class LoginScreen : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Create your application here
-            SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.LoginScreen);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            SetContentView(Resource.Layout.LoginScreen);
             Button login = FindViewById<Button>(Resource.Id.login);
 
             login.Click += (object sender, EventArgs e) =>
                 Android.Widget.Toast.MakeText(this, "Login Button Clicked!", Android.Widget.ToastLength.Short).Show();
-        }
+    }
     }
 }
+
--- Munch/Resources/Resource.Designer.cs
+++ Munch/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace Munch
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int logo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -61,7 +64,25 @@ namespace Munch
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int fuckingpat_1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int fuckingpat_2 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int fuckingpat_3 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int fuckingpat_4 = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int login = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int password = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int userName = 2131034116;
 			
 			static Id()
 			{
@@ -77,7 +98,10 @@ namespace Munch
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int AdminPortal = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int LoginScreen = 2130903041;
 			
 			static Layout()
 			{
