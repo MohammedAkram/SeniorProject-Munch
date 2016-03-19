dc32bc53ab8bea1f30376cb6dc9c37a6cf9195e5
Eric Mak <ericmak415@gmail.com>
2016-03-18 22:55:38 -0400

    Started implementing PubNub for Messaging

--- Munch.Droid/AdminPortal.cs
+++ Munch.Droid/AdminPortal.cs
@@ -9,23 +9,59 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using PubNubMessaging.Core;
 
 namespace Munch
 {
     [Activity(MainLauncher = true, Label = "AdminPortal", Theme = "@android:style/Theme.Holo.Light.NoActionBar", ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
     public class AdminPortal : Activity
     {
+        Pubnub pubnub = new Pubnub("sub-c-d70d769c-ebda-11e5-8112-02ee2ddab7fe", "pub-c-ddf91c9e-baf7-47af-8ca8-276337355d46");
 
         public override void OnBackPressed()
         {
             
             Android.Widget.Toast.MakeText(this, "You must logout to do that!", Android.Widget.ToastLength.Short).Show();
         }
+            void DisplaySubscribeReturnMessage(string result)
+        {
+            Console.WriteLine("SUBSCRIBE REGULAR CALLBACK:");
+            Console.WriteLine(result);
+            if (!string.IsNullOrEmpty(result) && !string.IsNullOrEmpty(result.Trim()))
+            {
+                List<object> deserializedMessage = pubnub.JsonPluggableLibrary.DeserializeToListOfObject(result);
+                if (deserializedMessage != null && deserializedMessage.Count > 0)
+                {
+                    object subscribedObject = (object)deserializedMessage[0];
+                    if (subscribedObject != null)
+                    {
+                        //IF CUSTOM OBJECT IS EXCEPTED, YOU CAN CAST THIS OBJECT TO YOUR CUSTOM CLASS TYPE
+                        string resultActualMessage = pubnub.JsonPluggableLibrary.SerializeToJsonString(subscribedObject);
+                    }
+                }
+            }
+        }
+        void DisplaySubscribeConnectStatusMessage(string result)
+        {
+            Console.WriteLine("SUBSCRIBE CONNECT CALLBACK");
+        }
+        void DisplayErrorMessage(PubnubClientError pubnubError)
+        {
+            Console.WriteLine(pubnubError.StatusCode);
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
+            pubnub.Subscribe<string>(
+            "my_channel",
+            DisplaySubscribeReturnMessage,
+            DisplaySubscribeConnectStatusMessage,
+            DisplayErrorMessage
+);
+
             base.OnCreate(savedInstanceState);
-         
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.AdminPortal);
 
@@ -52,6 +88,7 @@ namespace Munch
 
             //view reports button
             Button view_reports = FindViewById<Button>(Resource.Id.ViewReportsButton);
+
 
             view_reports.Click += delegate
             {
