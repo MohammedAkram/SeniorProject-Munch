dbf490faffb4c48f01585ae400e6ed93b3085897
MohammedAkram <makram01@nyit.edu>
2016-04-02 19:19:31 -0400

    add button to yourorder to send to MYSQL

--- Munch.Droid/CV2_YourOrder.cs
+++ Munch.Droid/CV2_YourOrder.cs
@@ -23,7 +23,7 @@ namespace Munch
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.CV_Your_Order);
             mListView = FindViewById<ListView>(Resource.Id.mngYour_Order_ListView);
-
+            Button orderUP = FindViewById<Button>(Resource.Id.ORderUP);
             //CustomerPortal.CustomerOrderList.Insert(0, (new CustomerOrderItem() { Dish = "dishname", Quantity = "test quant", Notes = "test note", OrderNumber = "" }));
             //  CustomerPortal.CustomerOrderList.Add(new CustomerOrderItem() { Dish = Menu.dishName_to_order, Quantity = "testq", Notes = "testn", OrderNumber = "" });
             // CustomerPortal.CustomerOrderList.Add(new CustomerOrderItem() { Dish = "huhui", Quantity = "testq", Notes = "testn", OrderNumber = "" });
@@ -32,7 +32,42 @@ namespace Munch
             CV_your_Order_ListViewAdapter adapter = new CV_your_Order_ListViewAdapter(this, (CustomerPortal.CustomerOrderList));
             Console.Out.WriteLine(CustomerPortal.CustomerOrderList + "******************************");
             mListView.Adapter = adapter;
+            //Orderbutton click
 
+            orderUP.Click += OrderUP_Click;
+
+        }
+        public static string Namee;
+        public static string quantityy;
+        public static string notesssssss;
+        public static int count = 0;
+        //order button 
+        private void OrderUP_Click(object sender, EventArgs e)
+        {
+
+            //loop into listview
+            for (int i = 0; i < mListView.Count; i++)
+            {
+                //gets each row view and stores it into V 
+                var v = mListView.Adapter.GetView(i, null, null);
+                //get the textview of the dish name
+                var dishname = (TextView)v.FindViewById(Resource.Id.Manage_Your_Order_Txt_Name);
+                var quantity = (TextView)v.FindViewById(Resource.Id.Manage_Your_Order_Txt_Quantity);
+                var notesss = (TextView)v.FindViewById(Resource.Id.Manage_Your_Order_Txt_Note);
+                //add the dish name to a static  string called m which can be used to store info in the database
+                Namee = (dishname.Text);
+                quantityy = (quantity.Text);
+                notesssssss = (notesss.Text);
+                var loginname = LoginScreen.loginUsername;
+
+                Console.WriteLine(Namee + "################################################3");
+
+                //NEED URL FOR ADDING TO MYSQL
+                //var webClient = new WebClient();
+                // webClient.DownloadString("HTTP FOR SENDING EACH ORDER IN THE LIST TO SPECIFIC TABLE IN MYSQL");
+            }
+
+            Android.Widget.Toast.MakeText(this, "ORDER UP, FOOD WILL BE READY IN 10 MIN", Android.Widget.ToastLength.Short).Show();
         }
     }
 }
